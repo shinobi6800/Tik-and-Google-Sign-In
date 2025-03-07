ebd2559437b7f47fc92459e70a1cd79cdec240cd
nige.dev <108910793+shinobi6800@users.noreply.github.com>
2025-03-07 02:53:17 +0000

    Update index.js
    Re read code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 const cors = require('cors');
 
-// Initialize Express
+// Initializing Express
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -73,7 +73,7 @@ app.post('/googlelogin', async (req, res) => {
     }
 });
 
-// Start the server
+// Start the server for basic backend
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
